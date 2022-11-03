82df618b436532874192da7e0b2d616be2cf81eb
lord-Art <dynearth99@gmail.com>
2022-11-03 14:54:54 +0700

    tugas 7

---
+++ 06.js
@@ -0,0 +1,11 @@
+function rentang (pertama, kedua){
+    let num = pertama;
+    console.log(pertama)
+    for(let i=pertama; i<kedua;i++){
+        num = i+1
+        
+        console.log(num)
+    }
+}
+
+rentang(4,8)
---
+++ 10.js
@@ -0,0 +1,4 @@
+const stuff = ['Meja', 'Buku', 'Topi', 'Baju', 'Kayu']
+stuff[0] ="Handuk"
+stuff[5] ="celana"
+console.log(stuff)
